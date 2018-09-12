81866d97768e950b2b30c5c2513d138a7be780ee
Cheng Lin <bnn1044@gmail.com>
2018-09-12 10:31:52 -0400

    ADD vscode support

---
+++ obj/main/STM32F3_DISCOVERY/main.d
@@ -0,0 +1,4 @@
+obj/main/STM32F3_DISCOVERY/main.o: src/main/main.cpp \
+ src/main/target/STM32F3_DISCOVERY/target.h
+
+src/main/target/STM32F3_DISCOVERY/target.h:
---
+++ obj/main/STM32F3_DISCOVERY/src/startup/startup_stm32f30x_md_gcc.d
@@ -0,0 +1,2 @@
+obj/main/STM32F3_DISCOVERY/./src/startup/startup_stm32f30x_md_gcc.o: \
+ src/startup/startup_stm32f30x_md_gcc.S
